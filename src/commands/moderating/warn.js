4d162554cf0978f26f99f16aafba24cd974b7835
Artoria01 <79462135+Artoria01@users.noreply.github.com>
2021-07-07 00:29:48 +0200

    Update warn.js

--- src/commands/moderating/warn.js
+++ src/commands/moderating/warn.js
@@ -4,28 +4,28 @@ module.exports = {
     config: {
         name: "warn",
         usage: "$warn <user> <reason>",
-        description: "Somebody misbehaving? Try warning them",
+        description: "Quelqu'un se comporte mal ? Essayez de les avertir",
         permissions: "administrator"
     },
     run: async (bot, message, args) => {
-        if (message.channel.type == "dm") return message.channel.send("This command only works in a server!");
+        if (message.channel.type == "dm") return message.channel.send("Cette commande ne fonctionne que sur un serveur!");
         if (!message.member.hasPermission("ADMINISTRATOR")) return errors.noPerms(message, "ADMINISTRATOR");
         
         let cmd = message.content.split(" ")[0]; //because command aliases
         if (args[0] == "help") return message.channel.send(`Usage: ${cmd} <user> <reason>`);
 
         let mutee = message.mentions.members.first() || message.guild.members.get(args[0]);
-        if (!mutee) return message.channel.send("User not found.");
+        if (!mutee) return message.channel.send("Utilisateur non trouvé.");
 
         if (mutee.id === bot.user.id) return errors.botuser(message, "warn");
 
         let reason = args.join(" ").slice(22);
-        if (!reason) reason = "No reason was given."
+        if (!reason) reason = "Aucune raison n'a été donnée."
         
         mutee.send(`You were warned in ${message.guild.name} for ${reason}`).catch(() => {
-            return message.channel.send(":x: That user has their dm's blocked");
+            return message.channel.send(":x: Cet utilisateur a son **DM** bloqué");
         });
-        return message.channel.send(`${mutee} ***has been warned!***`);
+        return message.channel.send(`${mutee} ***a été prévenu!***`);
 
     }
 }
